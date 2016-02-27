04e9caaa10f3478d965e68245782963d70b447d3
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-27 16:23:15 -0500

    fixed jQuery

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
-
-
+//changes background color behind photo
 $('.changeColor').click(function() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
   for (var i = 0; i < 6; i++ ) {
       color += letters[Math.floor(Math.random() * 16)];
   }
-      document.querySelector('div').style.backgroundColor = color;
+      $('div.imgbg').css("backgroundColor", color);
   });
