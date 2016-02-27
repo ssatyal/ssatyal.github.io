7f2e0014fc226718ff1634ddee90cb8cc60c0b9f
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-27 16:19:02 -0500

    added script, randomize color in html

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+$('.changeColor').click(function() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+      document.querySelector('div').style.backgroundColor = color;
+  });
