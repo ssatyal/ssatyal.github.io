a761b57edcc78689cd6c48fb55724a0d69784b1f
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-13 19:57:35 -0400

    jquery enhancements

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ $('.changeColor').click(function() {
   }
       $('div.imgbg').css("backgroundColor", color);
   });
+
+$('#aboutHeader').on('click',function(){
+  event.preventDefault();
+  $('#goeshere').replaceWith("<div id='goeshere'><section id='aboutMe'><p>Bacon ipsum dolor amet strip steak cow biltong bresaola,landjaeger filet mignon sirloin turducken picanha pork belly chicken prosciutto. Cow ground round pork loin bacon. Shank shoulder cupim pastrami pork swine kielbasa strip steak beef ribs rump hamburger corned beef salami meatloaf pancetta. Drumstick tongue bacon pancetta boudin jerky shank ribeye. Ham fatback swine meatloaf hamburger, pork ground round ball tip drumstick. Tail ribeye filet mignon sirloin. Boudin kielbasa rump ribeye chicken salami flank pork bacon picanhatri-tip.</p></section></div>");
+});
+
+$('#featured').on('click', function(){
+  event.preventDefault();
+  $('#goeshere').replaceWith(
+    '<div id="goeshere"><section id="projects"><a href="#">PlaceHolder 1</a><a href="#">PlaceHolder 2</a><a href="#">PlaceHolder 3</a></section></div>'
+  );
+});
+
+
+$('#contact').on('click',function(){
+  $('#goeshere').replaceWith('<div id="goeshere"><section id="contactinfo"><p><span>satyal89@gmail.com</span></p></section></div>');
+});
