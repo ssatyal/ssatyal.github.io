c0a64b1b55b507e49aa62dbb364116b25abbc18d
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-14 10:03:46 -0400

    added vars in script

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //changes background color behind photo
+var aboutHeader = $('#aboutHeader');
+var featured = $('#featured');
+var contact = $('#contact');
+
 $('.changeColor').click(function() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
@@ -8,12 +12,11 @@ $('.changeColor').click(function() {
       $('div.imgbg').css("backgroundColor", color);
   });
 
-$('#aboutHeader').on('click',function(){
-  event.preventDefault();
+aboutHeader.on('click',function(){
   $('#goeshere').replaceWith("<div id='goeshere'><section id='aboutMe'><p>Bacon ipsum dolor amet strip steak cow biltong bresaola,landjaeger filet mignon sirloin turducken picanha pork belly chicken prosciutto. Cow ground round pork loin bacon. Shank shoulder cupim pastrami pork swine kielbasa strip steak beef ribs rump hamburger corned beef salami meatloaf pancetta. Drumstick tongue bacon pancetta boudin jerky shank ribeye. Ham fatback swine meatloaf hamburger, pork ground round ball tip drumstick. Tail ribeye filet mignon sirloin. Boudin kielbasa rump ribeye chicken salami flank pork bacon picanhatri-tip.</p></section></div>");
 });
 
-$('#featured').on('click', function(){
+featured.on('click', function(){
   event.preventDefault();
   $('#goeshere').replaceWith(
     '<div id="goeshere"><section id="projects"><a href="#">PlaceHolder 1</a><a href="#">PlaceHolder 2</a><a href="#">PlaceHolder 3</a></section></div>'
@@ -21,6 +24,6 @@ $('#featured').on('click', function(){
 });
 
 
-$('#contact').on('click',function(){
+contact.on('click',function(){
   $('#goeshere').replaceWith('<div id="goeshere"><section id="contactinfo"><p><span>satyal89@gmail.com</span></p></section></div>');
 });
